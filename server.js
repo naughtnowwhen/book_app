6ea838841c97d797142271f9b22bf8a8518a1a3f
Peter Murphy <naughtnowwhen@gmail.com>
2019-05-09 18:35:07 -0700

    woking on getting data back from api

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require('express');
+const superagent = require('superagent');
 
 const app = express();
 const PORT= process.env.PORT || 3000;
@@ -16,9 +17,49 @@ app.get('/', (request, response)=>{
   response.render('pages/index');
 })
 
+app.post('/searches', searchGoogleBooks);
+
+
+function searchGoogleBooks (request, response) {
+
+  let url = 'https://www.googleapis.com/books/v1/volumes?q=';
+
+  console.log(request.body)
+  if (request.body.search[1]=== 'title') {url += `+intitle:${request.body.search[0]}`;}
+  if (request.body.search[1] === 'author') {url += `+inauthor:${request.body.search[0]}`;}
+
+  superagent.get(url)
+    .then(rawApiBookResponse => rawApiBookResponse.body.items.map(book => new Book(book.volumeInfo)))
+    .then(results => console.log(results));
+    // response.render('pages/searches/show', {searchResults : results}
+  }
+
+
+//what our constructor needs
+//title 
+//author 
+//isbn 
+//description 
+//image url 
+//bookshelf name
+
+//our book constructor (is this a helper function?)
+function Book(rawBookinfo) {
+  const placeholderImage = 'https://i.imgur.com/J5LVHEL.jpg';
+  this.title = rawBookinfo.title;
+  this.author = rawBookinfo.author;
+  this.isbn = rawBookinfo.isbn;
+  this.description = rawBookinfo.description;
+  // this.image_url = rawBookinfo.image_url
+  rawBookinfo.image_url ? this.image_url = rawBookinfo.image_url : placeholderImage;
+  // console.log(this);
+}
+
+
 // app.post('/contact', (request, response)=>{
 //   console.log('', {root: './public'})
 // });
+
 
 app.get('*', (request, response)=>response.status(404).send('This route does not exist'));
 
