9a3330ea71fe4a7025904a02ed8c8a4cc46eabd6
meron <malemu17@yahoo.com>
2019-05-13 15:46:13 -0700

    just-format

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 'use strict';
-//Application Dependencies
-
 //data\books.sql
+//Application Dependencies
 const express = require('express');
 const superagent = require('superagent');
 const pg = require('pg');
@@ -11,7 +10,6 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
 //Application Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('./public'));
@@ -19,11 +17,10 @@ app.use(express.static('./public'));
 //Database setup
 const client = new pg.Client(process.env.DATABASE_URL);
 client.connect();
-client.on('error', err=> console.error(err));
+client.on('error', err => console.error(err));
 
 //set the view engine for server side templating;
 app.set('view engine', 'ejs');
-
 
 //API Routes
 //Renders the search form
@@ -36,16 +33,12 @@ app.get('/', (request, response) => {
 app.get('/books/:book_id', getOneBook);
 app.get('/allstoredbooks', getAllStoredBooks);
 
-
 //Creates a new search to the Google Books API
 app.get('/prepareToSearchFromAPI', showTheSearchForm);
 app.post('/searches', searchGoogleBooks);
 
 app.get('add', showBook);
 app.post('/add', addBook);
-
-
-
 
 function searchGoogleBooks(request, response) {
 
@@ -60,7 +53,6 @@ function searchGoogleBooks(request, response) {
     .then(results => response.render('pages/searches/showSeveral', { searchResults: results }))
     .catch(err => handleError(err, response))
 }
-
 
 //what our constructor needs
 //title
@@ -80,51 +72,50 @@ function Book(rawBookinfo) {
   this.image_url = rawBookinfo.imageLinks ? rawBookinfo.imageLinks.thumbnail : 'https://i.imgur.com/J5LVHEL.jpg';
   rawBookinfo.industryIdentifiers[0].identifier ? this.ISBN_13 = rawBookinfo.industryIdentifiers[0].identifier : 'isbn unavailable';
   console.log(this.image_url[4]);
-  if (this.image_url[4] === ':'){
+  if (this.image_url[4] === ':') {
     this.image_url = this.image_url.replace('http', 'https');
   }
 }
 
-function addBook (request, response) {
+function addBook(request, response) {
   console.log('request body line 86............ ', request.body);
-  let {title, authors, description, image_url, ISBN_13} = request.body;
+  let { title, authors, description, image_url, ISBN_13 } = request.body;
   let SQL = `INSERT INTO books (title, authors, description, image_url, ISBN_13) VALUES ($1,$2,$3,$4,$5);`;
   let values = [title, authors, description, image_url, ISBN_13];
   return client.query(SQL, values)
-    .then(result=>{
+    .then(result => {
       console.log(result);
       response.redirect('/');
-    })
-    .catch(err=>handleError(err,response));
-}
-
-function showBook(request,response){
-  response.render('pages/show');
-}
-
-function getOneBook (request, response) {
-  console.log('book_id = ', request.params.book_id);
-  let SQL = `SELECT * FROM books where id=$1;`;
-  let values = [request.params.book_id];
-  return client.query(SQL,values)
-    .then(result=> {
-      return response.render('pages/showOne', {book : result.rows[0]});
     })
     .catch(err => handleError(err, response));
 }
 
-function showTheSearchForm (request, response) {
+function showBook(request, response) {
+  response.render('pages/show');
+}
+
+function getOneBook(request, response) {
+  console.log('book_id = ', request.params.book_id);
+  let SQL = `SELECT * FROM books where id=$1;`;
+  let values = [request.params.book_id];
+  return client.query(SQL, values)
+    .then(result => {
+      return response.render('pages/showOne', { book: result.rows[0] });
+    })
+    .catch(err => handleError(err, response));
+}
+
+function showTheSearchForm(request, response) {
   response.render('pages/searches/new');
 }
 
 function getAllStoredBooks(request, response) {
   let SQL = `SELECT * FROM books`;
   return client.query(SQL)
-    .then(result=>{
-      return response.render('pages/searches/allSavedBooks', {databaseResults : result.rows})
+    .then(result => {
+      return response.render('pages/searches/allSavedBooks', { databaseResults: result.rows })
     })
 }
-
 
 // app.post('/contact', (request, response)=>{
 //   console.log('', {root: './public'})
@@ -132,13 +123,23 @@ function getAllStoredBooks(request, response) {
 
 //catch-all
 
-
-function handleError(error, response){
-  response.render('pages/error/', {error: 'Something went wrong'});
+function handleError(error, response) {
+  response.render('pages/error/', { error: 'Something went wrong' });
 }
 
-app.get('*', (request, response)=>response.status(404).send('This route does not exist'));
+app.get('*', (request, response) => response.status(404).send('This route does not exist'));
 
 
-app.listen(PORT, ()=> console.log(`Listening on ${PORT}`));
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+
+
+
+
+
+
+
+
+
+
 
